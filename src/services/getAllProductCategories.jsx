19487abb3a3c127711d86bf6136f604df54228e5
DwiPutraA <124551419+22091397025DwiPutraAfrilianto@users.noreply.github.com>
2024-11-18 21:39:58 +0700

    Add files via upload

---
+++ src/services/getAllProductCategories.jsx
@@ -0,0 +1,15 @@
+const getAllProductCategories = ()  => {
+    return [   {
+      id: 'high',
+      slug: 'high',
+      name: `High`
+    },
+    {
+      id: 'low',
+      slug: 'low',
+      name: `Low`
+    },
+    ]
+  }
+  
+export default getAllProductCategories
---
+++ src/services/getAllProducts.js
@@ -0,0 +1,93 @@
+export default function getAllProducts() {
+  return [
+    {
+      id: 'ZR-001',
+      slug: 'Converse High 1',
+      name: "Converse High 1",
+      category: 'Sepatu',
+      categorySlug : 'Sepatu',
+      price: 999_900,
+      stock: 150,
+      imageUrl: '/assets/images/Gambar Sepatu tinggi 1.jpg',
+      description: `Sepatu berbahan Converse.`
+    },
+    {
+      id: 'ZR-002',
+      slug: 'Converse High 2',
+      name: "Converse High 2",
+      category: 'Sepatu',
+      categorySlug : 'Sepatu',
+      price: 1.299_900,
+      stock: 200,
+      imageUrl: '/assets/images/Gambar Sepatu tinggi 2.jpg',
+      description: `Sepatu berbahan Converse.`
+    },
+    {
+      id: 'ZR-003',
+      slug: 'Converse High 3',
+      name: "Converse High 3",
+      category: 'Sepatu',
+      categorySlug : 'Sepatu',
+      price: 1.499_900,
+      stock: 50,
+      imageUrl: `/assets/images/Gambar Sepatu tinggi 3.jpg`,
+      description: 'Sepatu berbahan Converse.'
+    },
+    {
+      id: 'ZR-004',
+      slug: 'Converse High 4',
+      name: "Converse High 4",
+      category: 'Sepatu',
+      categorySlug : 'Sepatu',
+      price: 899_900,
+      stock: 55,
+      imageUrl: `/assets/images/Gambar Sepatu tinggi 4.jpg`,
+      description: 'Sepatu berbahan Converse.'
+    },
+    {
+      id: 'ZR-005',
+      slug: 'Converse Low 1',
+      name: "Converse Low 1",
+      category: 'Sepatu',
+      categorySlug : 'Sepatu',
+      price: 899_900,
+      stock: 60,
+      imageUrl: `/assets/images/Gambar Sepatu pendek 1.jpg`,
+      description: `Sepatu berbahan Converse.`
+    },
+    {
+      id: 'ZR-006',
+      slug: 'Converse Low 2',
+      name: "Converse Low 2",
+      category: 'Sepatu',
+      categorySlug : 'Sepatu',
+      price: 699_900,
+      stock: 65,
+      imageUrl: `/assets/images/Gambar Sepatu pendek 2.jpg`,
+      description: `Sepatu berbahan Converse.`
+    },
+    {
+      id: 'ZR-007',
+      slug: 'Converse Low 3',
+      name: "Converse Low 3",
+      category: 'Sepatu',
+      categorySlug : 'Sepatu',
+      price: 1.499_900,
+      stock: 40,
+      imageUrl: `/assets/images/Gambar Sepatu pendek 3.jpg`,
+      description: `Sepatu berbahan Converse.`
+    },
+    {
+      id: 'ZR-008',
+      slug: 'Converse Low 4',
+      name: "Converse Low 4",
+      category: 'Sepatu',
+      categorySlug : 'Sepatu',
+      price: 1.299_900,
+      stock: 55,
+      imageUrl: `/assets/images/Gambar Sepatu pendek 4.jpg`,
+      description: `Sepatu berbahan Converse.`
+    },
+
+  ]
+}
